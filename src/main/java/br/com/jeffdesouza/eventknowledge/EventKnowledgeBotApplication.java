47846922c21fc4f58272e29a5d5878b5ad292a4b
Jeff de Souza <jeffdesouza.info@gmail.com>
2026-08-14 23:09:07 -0300

    chore: Bootstrap for Java + Spring Boot application.

---
+++ src/main/java/br/com/jeffdesouza/eventknowledge/EventKnowledgeBotApplication.java
@@ -0,0 +1,11 @@
+package br.com.jeffdesouza.eventknowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventKnowledgeBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventKnowledgeBotApplication.class, args);
+    }
+}
